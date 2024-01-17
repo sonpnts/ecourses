94a22579a359a6487a3b91b5181d5110e8954030
Phạm Ngọc Trường Sơn <sonpnts@ou.edu.vn>
2024-01-17 23:08:52 +0700

    hi

---
+++ courses/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='uploads/%Y/%m/%d/', null=True, blank=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True
+    subject = models.CharField(max_length=255, null=False)
+    image = models.ImageField(upload_to='lessons/%Y/%m', default=None)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class Course(ItemBase):
+    class Meta:
+        unique_together = ['subject', 'category']
+        ordering = ["-id"]
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Lesson(ItemBase):
+    class Meta:
+        unique_together = ['subject', 'course']
+    content = models.TextField()
+    course = models.ForeignKey(Course,related_name="lessons" , on_delete=models.CASCADE, null=True, blank=True)
+    tags= models.ManyToManyField('Tag', blank=True, null=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
