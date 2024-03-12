114ae93e02a227505156add0c8fe695644425bfb
admin <sonpnts@gmail.com>
2024-03-12 15:56:50 +0700

    upload

--- courses/course/models.py
+++ courses/course/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
 
 class User(AbstractUser):
     pass
@@ -7,7 +8,7 @@ class User(AbstractUser):
 
 class BaseModel(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
     class Meta:
@@ -22,9 +23,9 @@ class Category(BaseModel):
 
 class Course(BaseModel):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = RichTextField()
     image = models.ImageField(upload_to='course/%Y/%m/')
-    Category = models.ForeignKey(Category,on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
--- courses/courses/settings.py
+++ courses/courses/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,9 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'course.apps.CourseConfig'
+    'course.apps.CourseConfig',
+    'ckeditor',
+    'ckeditor_uploader'
 ]
 
+CKEDITOR_UPLOAD_PATH = "ckeditor/images"
 AUTH_USER_MODEL = 'course.User'
 MEDIA_ROOT = '%s/course/static/' % BASE_DIR
 
@@ -73,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'courses.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -87,8 +87,8 @@ DATABASES = {
     }
 }
 
-
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 # Password validation
@@ -109,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -120,7 +119,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
